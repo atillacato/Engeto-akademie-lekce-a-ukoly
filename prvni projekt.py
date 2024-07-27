c2ff68d92eb25a863505058b0a6fd79ad105e752
atillacato <atillacato@seznam.cz>
2024-07-27 19:32:26 +0200

    Add files via upload

---
+++ prvni projekt.py
@@ -0,0 +1,13 @@
+"""
+prvni_projekt.py: první projekt do Engeto Online Python Akademie
+
+author: Ladislav Junek
+email: junas@centrum.cz
+discord: lada_61655
+"""
+import ...
+registrovaný_uživatel = {
+    'bob' : 123
+    'ann' :
+}
+
